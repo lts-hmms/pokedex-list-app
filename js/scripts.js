0d9888774648e795af7d950a60117ede2b19359b
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-09-28 13:50:33 +0200

    chore: wrapped a24List array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,42 @@
-let a24List = [
-	{
-		name: 'The Witch',
-		year: 2015,
-		length: 92,
-		genre: [
-			'Drama',
-			'Fantasy',
-			'Horror'
-		]
-	},
-	{
-		name: 'Lady Bird',
-		year: '2017',
-		length: 94,
-		genre: [
-			'Comedy',
-			'Drama'
-		]
-	},
-	{
-		name: 'Moonlight',
-		year: 2016,
-		length: 111,
-		types: [
-			'Drama'
-		]
+let a24Repository = (function() {
+	let a24List = [
+		{
+			name: 'The Witch',
+			year: 2015,
+			length: 92,
+			genre: [
+				'Drama',
+				'Fantasy',
+				'Horror'
+			]
+		},
+		{
+			name: 'Lady Bird',
+			year: '2017',
+			length: 94,
+			genre: [
+				'Comedy',
+				'Drama'
+			]
+		},
+		{
+			name: 'Moonlight',
+			year: 2016,
+			length: 111,
+			types: [
+				'Drama'
+			]
+		}
+	];
+	function add(movie) {
+		a24List.push(movie);
 	}
-];
+
+	function getAll() {
+		return a24List;
+	}
+})();
+
 /*iterates over a24List and writes down each name and length,
 when length is over 100 (minutes), then "Wow, it's long" is added. */
 
