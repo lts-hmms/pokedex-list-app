999d7890689d143a71489d1b23bc801de9054b68
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-10-01 19:38:00 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+//IIFE
+let a24Repository = (function() {
+	let movieList = [
+		{
+			name: 'The Witch',
+			year: '2015',
+			length: 92,
+			genre: [
+				'Drama',
+				'Fantasy',
+				'Horror'
+			]
+		},
+		{
+			name: 'Lady Bird',
+			year: '2017',
+			length: 94,
+			genre: [
+				'Comedy',
+				'Drama'
+			]
+		},
+		{
+			name: 'Moonlight',
+			year: '2016',
+			length: 111,
+			genre: [
+				'Drama'
+			]
+		}
+	];
+	// function for adding a movie
+	function add(movie) {
+		if (typeof movie === 'object' && 'name' in movie && 'year' in movie && 'length' in movie && 'genre' in movie) {
+			movieList.push(movie);
+		} else {
+			return 'invalid input';
+		}
+	}
+	// function to show all movies
+	function getAll() {
+		return movieList;
+	}
+
+	// function to find a specific movie
+	function findMovie(userInput) {
+		return movieList.filter((movie) => movie.name.toLowerCase() === userInput.toLowerCase());
+	}
+
+	//function which creates buttons for each movie including showDetails of movie by click
+	function addListItem(movie) {
+		let list = document.querySelector('.movie-list');
+		let listItem = document.createElement('li');
+		let button = document.createElement('button');
+		button.innerText = movie.name;
+		button.classList.add('name-button');
+		listItem.appendChild(button);
+		list.appendChild(listItem);
+		list.classList.add('list-class');
+		button.classList.add('button-class');
+		button.addEventListener('click', function() {
+			showDetails(movie);
+		});
+	}
+
+	function showDetails(movie) {
+		console.log(movie);
+	}
+
+	return {
+		add: add,
+		getAll: getAll,
+		findMovie: findMovie,
+		addListItem: addListItem,
+		showDetails: showDetails
+	};
+})();
+
+a24Repository.getAll().forEach(function(movie) {
+	a24Repository.addListItem(movie);
+});
